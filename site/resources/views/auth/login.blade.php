ea25bcead33f328809d6419978721c4e48f3ab7e
kefirzhang <8390671@qq.com>
2019-07-16 19:22:58 +0800

    develop batch commit

--- site/resources/views/auth/login.blade.php
+++ site/resources/views/auth/login.blade.php
@@ -104,59 +104,11 @@ License: You must have a valid license purchased only from themeforest(the above
                             </div>
                         </form>
                     </div>
-                    <div class="kt-login__signup">
-                        <div class="kt-login__head">
-                            <h3 class="kt-login__title">Sign Up</h3>
-                            <div class="kt-login__desc">Enter your details to create your account:</div>
-                        </div>
-                        <form class="kt-login__form kt-form" action="">
-                            <div class="input-group">
-                                <input class="form-control" type="text" placeholder="Fullname" name="fullname">
-                            </div>
-                            <div class="input-group">
-                                <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
-                            </div>
-                            <div class="input-group">
-                                <input class="form-control" type="password" placeholder="Password" name="password">
-                            </div>
-                            <div class="input-group">
-                                <input class="form-control" type="password" placeholder="Confirm Password" name="rpassword">
-                            </div>
-                            <div class="row kt-login__extra">
-                                <div class="col kt-align-left">
-                                    <label class="kt-checkbox">
-                                        <input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.
-                                        <span></span>
-                                    </label>
-                                    <span class="form-text text-muted"></span>
-                                </div>
-                            </div>
-                            <div class="kt-login__actions">
-                                <button id="kt_login_signup_submit" class="btn btn-pill kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
-                                <button id="kt_login_signup_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="kt-login__forgot">
-                        <div class="kt-login__head">
-                            <h3 class="kt-login__title">Forgotten Password ?</h3>
-                            <div class="kt-login__desc">Enter your email to reset your password:</div>
-                        </div>
-                        <form class="kt-form" action="">
-                            <div class="input-group">
-                                <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
-                            </div>
-                            <div class="kt-login__actions">
-                                <button id="kt_login_forgot_submit" class="btn btn-pill kt-login__btn-primary">Request</button>&nbsp;&nbsp;
-                                <button id="kt_login_forgot_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
-                            </div>
-                        </form>
-                    </div>
                     <div class="kt-login__account">
 								<span class="kt-login__account-msg">
 									Don't have an account yet ?
 								</span>&nbsp;&nbsp;
-                        <a href="javascript:;" id="kt_login_signup" class="kt-link kt-link--light kt-login__account-link">Sign Up</a>
+                        <a href="{{ route('register') }}" id="kt_login_signup" class="kt-link kt-link--light kt-login__account-link">Sign Up</a>
                     </div>
                 </div>
             </div>
@@ -169,7 +121,6 @@ License: You must have a valid license purchased only from themeforest(the above
 @include('include.global_js')
 
 <!--begin::Page Scripts(used by this page) -->
-<script src="./assets/js/demo1/pages/login/login-general.js" type="text/javascript"></script>
 
 <!--end::Page Scripts -->
 </body>
--- site/resources/views/auth/register.blade.php
+++ site/resources/views/auth/register.blade.php
@@ -1,77 +1,152 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<!--
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 8
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<!-- begin::Head -->
+<head>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <!--begin::Base Path (base relative path for assets of this page) -->
+    <base href="../_static/">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    <!--end::Base Path -->
+    <meta charset="utf-8" />
+    <title>Metronic | Login Page 2</title>
+    <meta name="description" content="Login page example">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!--begin::Fonts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
+
+    <!--end::Fonts -->
+
+    <!--begin::Page Custom Styles(used by this page) -->
+    <link href="./assets/css/demo1/pages/login/login-2.css" rel="stylesheet" type="text/css" />
+
+    <!--end::Page Custom Styles -->
+
+    @include('include.global_css')
+    <style>
+        .kt-login.kt-login--v2 .kt-login__wrapper .kt-login__container .kt-form .form-control {
+            height: 46px;
+            border-radius: 46px;
+            border: none;
+            padding-left: 1.5rem;
+            padding-right: 1.5rem;
+            margin-top: 1.5rem;
+            background: #b0bade;
+        }
+
+    </style>
+</head>
+
+<!-- end::Head -->
+
+<!-- begin::Body -->
+<body class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--enabled kt-subheader--fixed kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading">
+
+<!-- begin:: Page -->
+<div class="kt-grid kt-grid--ver kt-grid--root">
+    <div class="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v2 kt-login--signin" id="kt_login">
+        <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" style="background-image: url(./assets/media//bg/bg-1.jpg);">
+            <div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
+                <div class="kt-login__container">
+                    <div class="kt-login__logo">
+                        <a href="#">
+                            <img src="./assets/media/logos/logo-mini-2-md.png">
+                        </a>
+                    </div>
+                    <div>
+                        <div class="kt-login__head">
+                            <h3 class="kt-login__title">Sign Up</h3>
+                            <div class="kt-login__desc">Enter your details to create your account:</div>
+                        </div>
+                        <form class="kt-login__form kt-form" method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="name"  name="name" value="{{ old('name') }}">
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div id="fullname-error" class="error invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Email" name="email" value="{{ old('email') }}" autocomplete="off">
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div id="email-error" class="error invalid-feedback" style="display: block;">{{ $message }}</div>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <div class="input-group">
+                                <input class="form-control" type="password" placeholder="Password" name="password">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div id="password-error" class="error invalid-feedback" style="display: block;">{{ $message }}</div>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="input-group">
+                                <input class="form-control" type="password" placeholder="Confirm Password" name="password_confirmation">
+                                @error('password_confirmation')
+                                <div id="rpassword-error" class="error invalid-feedback">{{ $message }}</div>
+                                @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            <div class="row kt-login__extra">
+                                <div class="col kt-align-left">
+                                    <label class="kt-checkbox">
+                                        <input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.
+                                        <span></span>
+                                        @error('agree')
+                                        <div id="agree-error" class="error invalid-feedback" style="display: block;">{{ $message }}</div>
+                                        @enderror
+                                    </label>
+                                    <span class="form-text text-muted"></span>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                            <div class="kt-login__actions">
+                                <button id="kt_login_signup_submit" class="btn btn-pill kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
+                                <button id="kt_login_signup_cancel" class="btn btn-pill kt-login__btn-secondary">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="kt-login__account">
+								<span class="kt-login__account-msg">
+									Don't have an account yet ?
+								</span>&nbsp;&nbsp;
+                        <a href="{{ route('register') }}" id="kt_login_signup" class="kt-link kt-link--light kt-login__account-link">Sign Up</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<!-- end:: Page -->
+
+@include('include.global_js')
+
+<!--begin::Page Scripts(used by this page) -->
+
+<!--end::Page Scripts -->
+</body>
+
+<!-- end::Body -->
+</html>
