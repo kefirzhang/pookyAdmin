c1dd197f40f5edb928424626ec59cca470ad46b0
kefirzhang <kefirzhang@tencent.com>
2020-03-22 13:29:56 +0800

    注释 google.api

--- site/resources/views/auth/login.blade.php
+++ site/resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!--begin::Fonts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>-->
     <script>
         WebFont.load({
             google: {
